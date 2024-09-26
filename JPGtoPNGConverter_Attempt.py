952db6511c5b149feb1534e3e0fb31c5c4abdffe
Elysia Kilayko <elysiamk@gmail.com>
2024-09-26 10:53:26 +1000

    making minor updates to the script

--- JPGtoPNGConverter_Attempt.py
+++ JPGtoPNGConverter_Attempt.py
@@ -7,13 +7,15 @@ from PIL import Image
 
 #to change the directory in the terminal window: Set-Location -Path "file path name"
 #Set-Location -Path "C:\Users\elysia.kilayko\OneDrive\Documents\VS Code\Learning\Python\ZTM\pokedex"
+#or
+#cd "C:\Users\elysia.kilayko\OneDrive\Documents\VS Code\Learning\Python\ZTM\pokedex"
 
 # currentDirectory = os.path.dirname(__file__)
 # os.chdir(currentDirectory)
 # print(currentDirectory)
 
 #grab the first and second argument (\Pokedex and \new), which are inputted in the terminal window
-#full input into the terminal window: python JPGtoPNGConverter2.py Pokedex New
+#full input into the terminal window: python JPGtoPNGConverter_Attempt.py Pokedex New
 
 jpgLocation = sys.argv[1]
 pngLocation = sys.argv[2]
--- images.py
+++ images.py
@@ -9,12 +9,12 @@ from PIL import Image, ImageFilter
 #     print(img2)
 
 #the following prints out the whole jpeg information and creates a representation of the image in memory
-img = Image.open("./ZTM/pokedex/Pokedex/pikachu.jpg")
+img = Image.open("./Pokedex/pikachu.jpg")
 filtered_img = img.filter(ImageFilter.BLUR)
 
 #turns picture into grey scale
 filtered_img = img.convert("L")
-filtered_img.save("./ZTM/pokedex/grey.png")
+filtered_img.save("./pokedex/grey.png")
 
 #resizing
 resize = filtered_img.resize((300,300))
